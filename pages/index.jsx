f1a96a1e9ce0ad8fbd2e968d6950f638fb8a4877
Sean Dawson <contact@seandawson.info>
2019-04-23 21:32:32 +1000

    Implement core BS generator functionallity

---
+++ pages/index.jsx
@@ -0,0 +1,60 @@
+import { verbs, adjectives, nouns } from "../modules/words";
+import _ from "lodash";
+import React from "react";
+import Head from "next/head";
+
+// TODO: Importing SASS seems to hang the runner
+import "../assets/css/main.css";
+import "../assets/css/noscript.css";
+
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bs: this.generateBsString()
+    };
+  }
+
+  generateBsString() {
+    return `${_.sample(verbs)} ${_.sample(adjectives)} ${_.sample(nouns)}`;
+  }
+
+  refreshBsState() {
+    const bs = this.generateBsString();
+    this.setState(state => ({
+      bs
+    }));
+  }
+
+  handleClick() {
+    this.refreshBsState();
+  }
+
+  render() {
+    return (
+      <div id="page-wrapper">
+        <Head>
+          <title>NGO BS Generator</title>
+        </Head>
+        <section id="banner">
+          <div className="inner">
+            <h2>NGO BS Generator</h2>
+            <p>{this.state.bs}</p>
+            <ul className="actions special">
+              <li>
+                <button
+                  className="button primary"
+                  onClick={this.handleClick.bind(this)}
+                >
+                  Generate
+                </button>
+              </li>
+            </ul>
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
+
+export default Home;
